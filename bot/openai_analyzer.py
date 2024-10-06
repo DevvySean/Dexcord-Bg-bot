753e206df8a08083ae92ab19acd4617890364337
Sean <107492234+DevvySean@users.noreply.github.com>
2024-10-06 11:55:34 +0100

    pushing changes

--- bot/openai_analyzer.py
+++ bot/openai_analyzer.py
@@ -11,8 +11,8 @@ def analyze_blood_sugar_trends(readings, time_range='24h'):
     reading_summary = "\n".join([f"Time: {reading['timestamp']}, Blood Sugar: {reading['blood_glucose_value']}, Trend: {reading['description']}" for reading in readings])
     
     prompt = f"""Here are the blood sugar readings from the last {time_range}:\n\n{reading_summary}\n\nCan you analyze these and provide insights on any trends, 
-    also make sure you have enough data to make a good analysis and provide recommendations for the user. If you dont have enough data please let the user know and dont suggest anything.
-    also can you provide a summary of the trends in the blood sugar readings and give the user their percentage for time their blood sugars were in range, their range is between 5 and 10 mmol/L."""
+    also make sure you have enough data to make a good analysis. Can you also provide a deeper insight into the time period of the trends. Also can you provide a summary of the trends in the blood sugar 
+    readings and give the user their percentage for time their blood sugars were in range, their range is between 5 and 10 mmol/L."""
     messages = [
         {
             "role": "system",
@@ -27,7 +27,7 @@ def analyze_blood_sugar_trends(readings, time_range='24h'):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
-        max_tokens=300,
+        max_tokens=500,
         temperature=0
     )
 
